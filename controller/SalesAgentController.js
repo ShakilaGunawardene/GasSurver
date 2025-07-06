0a308893fd8ff169692c534078350a2b3a411e58
Shakila <shakilaqs96@gmail.com>
2025-07-06 22:28:47 +0530

    finish Sales Agent part

--- controller/SalesAgentController.js
+++ controller/SalesAgentController.js
@@ -5,7 +5,7 @@ const registerSalesAgent = async (req, res) => {
   const { salesAgentId, salesAgentName, salesAgentGasBrandName, salesAgentGasType, salesAgentEmail,salesAgentPassword } = req.body;
   try {
     const exists = await SalesAgent.findOne({ salesAgentEmail });
-    if (exists) return res.status(400).json({ message: 'Driver already exists' });
+    if (exists) return res.status(400).json({ message: 'SalesAgent already exists' });
 
     const salesAgent = new SalesAgent({ salesAgentId, salesAgentName, salesAgentGasBrandName, salesAgentGasType, salesAgentEmail,salesAgentPassword });
     await salesAgent.save();
@@ -15,10 +15,58 @@ const registerSalesAgent = async (req, res) => {
   }
 };
 
+// Read all SalesAgent
+const getAllSalesAgent = async (req, res) => {
+  try {
+    const salesAgent = await SalesAgent.find();
+    res.json(salesAgent);
+  } catch (err) {
+    res.status(500).json({ message: 'Error', error: err.message });
+  }
+};
+
+// Read SalesAgent by ID
+const getSalesAgentById = async (req, res) => {
+  try {
+    const salesAgent = await SalesAgent.findById(req.params.id);
+    if (!salesAgent) return res.status(404).json({ message: 'User not found' });
+    res.json(salesAgent);
+  } catch (err) {
+    res.status(500).json({ message: 'Error', error: err.message });
+  }
+};
+
+// Update SalesAgent
+const updateSalesAgent = async (req, res) => {
+  const { salesAgentId, salesAgentName, salesAgentGasBrandName, salesAgentGasType, salesAgentEmail,salesAgentPassword } = req.body;
+  try {
+    const updated = await SalesAgent.findByIdAndUpdate(
+      req.params.id,
+      { salesAgentId, salesAgentName, salesAgentGasBrandName, salesAgentGasType, salesAgentEmail,salesAgentPassword},
+      { new: true }
+    );
+    if (!updated) return res.status(404).json({ message: 'Sales Agents not found' });
+    res.json({ message: 'Sales Agents  updated', driver: updated });
+  } catch (err) {
+    res.status(500).json({ message: 'Error', error: err.message });
+  }
+};
+
+// Delete SalesAgent
+const deleteSalesAgent = async (req, res) => {
+  try {
+    const deleted = await SalesAgent.findByIdAndDelete(req.params.id);
+    if (!deleted) return res.status(404).json({ message: 'SalesAgent not found' });
+    res.json({ message: 'SalesAgent deleted', user: deleted });
+  } catch (err) {
+    res.status(500).json({ message: 'Error', error: err.message });
+  }
+};
+
 module.exports = {
   registerSalesAgent,
-//   getAllUsers,
-//   getUserById,
-//   updateUser,
-//   deleteUser
+  getAllSalesAgent,
+  getSalesAgentById,
+  updateSalesAgent,
+  deleteSalesAgent
 };
--- routes/SalesAgentRoutes.js
+++ routes/SalesAgentRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {registerSalesAgent, } = require("../controller/SalesAgentController");
+const {registerSalesAgent, getAllSalesAgent, getSalesAgentById, updateSalesAgent, deleteSalesAgent} = require("../controller/SalesAgentController");
 
 //Route:
 router.post('/register',registerSalesAgent);
+router.get('/getAllUsers',getAllSalesAgent);
+router.get('/getUserById',getSalesAgentById);
+router.put('/updateUser',updateSalesAgent);
+router.delete('/deleteUser',deleteSalesAgent);
 
 module.exports = router;
